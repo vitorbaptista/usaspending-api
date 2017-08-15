4719a47c7afc917a3dd2f9e24df1f945168bb9b4
TheRJB <bodine_russell@bah.com>
2017-08-15 11:22:53 -0400

    bug fix 2

--- usaspending_api/etl/award_helpers.py
+++ usaspending_api/etl/award_helpers.py
@@ -236,7 +236,7 @@ def get_awarding_agency(row):
     if row.txn:
         # We found a matching transaction, so grab its awarding agency
         # info and pass it get_or_create_summary_award
-        return row.txn.awarding_agency
+        return row.txn['awarding_agency']
     else:
         # No matching transaction found, so find/create Award by using
         # topiter agency only, since CGAC code is the only piece of
