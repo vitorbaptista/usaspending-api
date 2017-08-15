1a0cb5a31b2cc10022d37fe45c492a7fd7d0285d
Daniel Boos <boos_daniel@bah.com>
2017-08-15 12:03:25 -0400

    Removing logs

--- usaspending_api/etl/award_helpers.py
+++ usaspending_api/etl/award_helpers.py
@@ -236,11 +236,7 @@ def get_awarding_agency(row):
     if row.txn:
         # We found a matching transaction, so grab its awarding agency
         # info and pass it get_or_create_summary_award
-        import logging
-        logger = logging.getLogger('console')
-        row.txn['awarding_agency'] = Agency.objects.get(id=row.txn['awarding_agency'])
-        logger.info("txn['awarding_agency']: {}".format(row.txn['awarding_agency']))
-        return row.txn['awarding_agency']
+        return Agency.objects.get(id=row.txn['awarding_agency'])
     else:
         # No matching transaction found, so find/create Award by using
         # topiter agency only, since CGAC code is the only piece of
