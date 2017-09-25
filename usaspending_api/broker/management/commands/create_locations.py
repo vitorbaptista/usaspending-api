be532b66c08fb76477496ddce00f6684b6130600
TheRJB <bodine_russell@bah.com>
2017-09-25 17:10:21 -0400

    Changed to only create, added save argument

--- usaspending_api/broker/management/commands/create_locations.py
+++ usaspending_api/broker/management/commands/create_locations.py
@@ -3,21 +3,13 @@ import timeit
 from datetime import datetime
 
 from django.core.management.base import BaseCommand
-from django.db import connections, transaction as db_transaction, IntegrityError
+from django.db import connections, transaction as db_transaction
 
 from usaspending_api.etl.broker_etl_helpers import dictfetchall
-from usaspending_api.awards.models import TransactionNormalized, TransactionFABS, TransactionFPDS
-from usaspending_api.awards.models import Award
-from usaspending_api.references.models import Agency, LegalEntity, SubtierAgency, ToptierAgency
-from usaspending_api.etl.management.load_base import copy, get_or_create_location, format_date, load_data_into_model
-from usaspending_api.etl.award_helpers import update_awards, update_contract_awards, update_award_categories
-import sys
+from usaspending_api.references.models import Agency, SubtierAgency, ToptierAgency, Location, RefCountryCode
+from usaspending_api.etl.management.load_base import copy, load_data_into_model
 
-# start = timeit.default_timer()
-# function_call
-# end = timeit.default_timer()
-# time elapsed = str(end - start)
-
+from usaspending_api.references.helpers import canonicalize_location_dict
 
 logger = logging.getLogger('console')
 exception_logger = logging.getLogger("exceptions")
@@ -39,7 +31,7 @@ class Command(BaseCommand):
     help = "Create Locations from Location data in the Broker."
 
     @staticmethod
-    def update_transaction_assistance(db_cursor, fiscal_year=None, page=1, limit=500000):
+    def update_transaction_assistance(db_cursor, fiscal_year=None, page=1, limit=500000, save=True):
 
         # logger.info("Getting IDs for what's currently in the DB...")
         # current_ids = TransactionFABS.objects
@@ -120,6 +112,7 @@ class Command(BaseCommand):
         logger.info("Processing " + str(total_rows) + " rows of assistance data")
 
         # skip_count = 0
+        bulk_array = []
 
         start_time = datetime.now()
         for index, row in enumerate(award_financial_assistance_data, 1):
@@ -137,22 +130,27 @@ class Command(BaseCommand):
                                                                                  str(total_rows),
                                                                                  datetime.now() - start_time))
 
-                lel, lel_created= get_or_create_location(
+                lel= get_or_create_location_pre_bulk(
                     legal_entity_location_field_map, row, legal_entity_location_value_map
                 )
 
                 # Create the place of performance location
-                pop, pop_created = get_or_create_location(
+                pop = get_or_create_location_pre_bulk(
                     place_of_performance_field_map, row, place_of_performance_value_map
                 )
 
-                if lel_created:
-                    lel.save()
-                if pop_created:
-                    pop.save()
+                if lel:
+                    bulk_array.append(lel)
+                if pop:
+                    bulk_array.append(pop)
+                Location.objects.bulk_create(bulk_array)
+                if fiscal_year:
+                    for l in bulk_array:
+                        l.save()
+
 
     @staticmethod
-    def update_transaction_contract(db_cursor, fiscal_year=None, page=1, limit=500000):
+    def update_transaction_contract(db_cursor, fiscal_year=None, page=1, limit=500000, save=True):
 
         # logger.info("Getting IDs for what's currently in the DB...")
         # current_ids = TransactionFPDS.objects
@@ -221,7 +219,7 @@ class Command(BaseCommand):
 
         logger.info("Processing " + str(total_rows) + " rows of procurement data")
 
-        # skip_count = 0
+        bulk_array = []
 
         start_time = datetime.now()
         for index, row in enumerate(procurement_data, 1):
@@ -238,18 +236,22 @@ class Command(BaseCommand):
                                                                                  str(total_rows),
                                                                                  datetime.now() - start_time))
 
-                lel, lel_created = get_or_create_location(
+                lel = get_or_create_location_pre_bulk(
                     legal_entity_location_field_map, row, copy(legal_entity_location_value_map)
                 )
+                if lel:
+                    bulk_array.append(lel)
 
                 # Create the place of performance location
-                pop, pop_created = get_or_create_location(
+                pop = get_or_create_location_pre_bulk(
                     place_of_performance_field_map, row, copy(place_of_performance_value_map))
+                if pop:
+                    bulk_array.append(pop)
 
-                if lel_created:
-                    lel.save()
-                if pop_created:
-                    pop.save()
+                Location.objects.bulk_create(bulk_array)
+                if save:
+                    for l in bulk_array:
+                        l.save()
 
     def add_arguments(self, parser):
 
@@ -292,6 +294,12 @@ class Command(BaseCommand):
             type=int,
             help="Limit for batching and parallelization"
         )
+        parser.add_argument(
+            '--save',
+            dest="save",
+            default=True,
+            help="Decides if the save method is called after loading"
+        )
 
     # @transaction.atomic
     def handle(self, *args, **options):
@@ -301,6 +309,7 @@ class Command(BaseCommand):
         fiscal_year = options.get('fiscal_year')
         page = options.get('page')
         limit = options.get('limit')
+        save = options.get('save')
 
         if fiscal_year:
             fiscal_year = fiscal_year[0]
@@ -314,14 +323,14 @@ class Command(BaseCommand):
         if not options['assistance']:
             logger.info('Starting D1 historical data location insert...')
             start = timeit.default_timer()
-            self.update_transaction_contract(db_cursor=db_cursor, fiscal_year=fiscal_year, page=page, limit=limit)
+            self.update_transaction_contract(db_cursor=db_cursor, fiscal_year=fiscal_year, page=page, limit=limit, save=save)
             end = timeit.default_timer()
             logger.info('Finished D1 historical data location insert in ' + str(end - start) + ' seconds')
 
         if not options['contracts']:
             logger.info('Starting D2 historical data location insert...')
             start = timeit.default_timer()
-            self.update_transaction_assistance(db_cursor=db_cursor, fiscal_year=fiscal_year, page=page, limit=limit)
+            self.update_transaction_assistance(db_cursor=db_cursor, fiscal_year=fiscal_year, page=page, limit=limit, save=save)
             end = timeit.default_timer()
             logger.info('Finished D2 historical data location insert in ' + str(end - start) + ' seconds')
 
@@ -336,7 +345,7 @@ class Command(BaseCommand):
         # update_contract_awards(tuple(award_contract_update_id_list))
         # end = timeit.default_timer()
         # logger.info('Finished updating contract specific awards in ' + str(end - start) + ' seconds')
-        # 
+        #
         # logger.info('Updating award category variables...')
         # start = timeit.default_timer()
         # update_award_categories(tuple(award_update_id_list))
@@ -345,3 +354,59 @@ class Command(BaseCommand):
 
         # Done!
         logger.info('FINISHED')
+
+
+def get_or_create_location_pre_bulk(location_map, row, location_value_map):
+    """
+    Retrieve or create a location object
+
+    Input parameters:
+        - location_map: a dictionary with key = field name on the location model
+            and value = corresponding field name on the current row of data
+        - row: the row of data currently being loaded
+    """
+    if location_value_map is None:
+        location_value_map = {}
+
+    row = canonicalize_location_dict(row)
+
+    location_country = RefCountryCode.objects.filter(
+        country_code=row[location_map.get('location_country_code')]).first()
+
+    state_code = row.get(location_map.get('state_code'))
+    if state_code is not None:
+        # Remove . in state names (i.e. D.C.)
+        location_value_map.update({'state_code': state_code.replace('.', '')})
+
+    if location_country:
+        location_value_map.update({
+            'location_country_code': location_country,
+            'country_name': location_country.country_name,
+            'state_code': None,  # expired
+            'state_name': None,
+        })
+    else:
+        # no country found for this code
+        location_value_map.update({
+            'location_country_code': None,
+            'country_name': None
+        })
+
+    location_data = load_data_into_model(
+        Location(), row, value_map=location_value_map, field_map=location_map, as_dict=True)
+
+    del location_data['data_source']  # hacky way to ensure we don't create a series of empty location records
+    if len(location_data):
+        if len(location_data) == 1 and "place_of_performance_flag" in location_data and \
+                location_data["place_of_performance_flag"]:
+            location_object = None
+            return location_object
+        try:
+            location_object = Location.objects.create(**location_data)
+            return location_object
+        except Exception:
+            logger.info('Exception in create location')
+            return None
+    else:
+        # record had no location information at all
+        return None
